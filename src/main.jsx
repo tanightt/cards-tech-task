cc04fa15590ff0b48e31e394abc0f39c527d4e13
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-07-30 18:45:22 +0300

    UPDATE: routes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/cards-tech-task">
     <Provider store={store}>
       <App />
     </Provider>
